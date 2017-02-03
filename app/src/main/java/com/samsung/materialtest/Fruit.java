4bf8bf1c302d556164b268e68e3353b7a86d8cf3
BobMing <bobmingxie@vip.qq.com>
2017-02-04 00:51:42 +0800

    Finish 12.5

---
+++ app/src/main/java/com/samsung/materialtest/Fruit.java
@@ -0,0 +1,23 @@
+package com.samsung.materialtest;
+
+/**
+ * Created by Admin on 2017/2/4 0004.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
